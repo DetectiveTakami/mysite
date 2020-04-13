24a5f8bf39040dff6683f09f5151ee5e486fc142
DetectiveTakami <61905831+DetectiveTakami@users.noreply.github.com>
2020-04-13 18:35:24 +0900

    Add files via upload

---
+++ service_worker.js
@@ -0,0 +1,27 @@
+//キャッシュファイル指定
+var CASH_NAME = 'pwa-sample-caches';
+var urlToCache= [
+  '/poster-keisuke.github.io/',
+];
+
+//インストール処理
+self.addEventListener('install', function(event) {
+  event.witUntil(
+    caches
+      .open(CASH_NAME)
+      .then(function(cache) {
+        return cache.addALL(urlsToCache);
+      })
+  );
+});
+
+//リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+  event.respondWidth(
+    caches
+    .match(event.request)
+    .then(function(response) {
+      return response ? response : fetch(event.request);
+    })
+  );
+});
